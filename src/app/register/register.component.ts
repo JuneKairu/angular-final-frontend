780e9ff18da94b825a3c7247ce30dca9a1c8b8a8
JKairu <micadario0926@gmail.com>
2024-06-14 22:02:50 +0800

    heh

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import axios from 'axios';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  name: string;
-  email: string;
-  password: string;
+  name!: string;
+  email!: string;
+  password!: string;
 
   constructor() {}
 
@@ -23,11 +23,9 @@ export class RegisterComponent {
     axios.post('YOUR_BACKEND_ENDPOINT/register', userData)
     .then(response => {
         console.log(response.data);
-        // Handle success (e.g., navigate to another page)
       })
     .catch(error => {
         console.error('There was an error!', error);
-        // Handle error (e.g., show an error message)
       });
   }
 }
